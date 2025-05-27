1fb827d4a771354c2d607665c6e021c16c639e03
Brandon Nascimento <brandonnascimento112@gmail.com>
2025-05-26 20:35:24 -0400

    tested JS features, got exposed to DOM manipulation with form basic submission

--- Javascript/main.js
+++ Javascript/main.js
@@ -227,4 +227,64 @@ console.log(person1);
 
 
 //DOM
+//Single element
+// const form = document.getElementById('my-form');
+// console.log(form);
+// console.log(document.querySelector('#my-form'));
+
+// //Multiple element
+// // console.log(document.querySelectorAll('.item'));
+// const items = document.querySelectorAll('.items');
+// items.forEach((item) => {
+//     console.log(item);
+// });
+
+// //Manipulate
+// const ul = document.querySelector('.items');
+// // ul.remove();
+// // ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = "Hello";
+// ul.children[1].innerText = 'Brad';
+// ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+// const btn = document.querySelector('.btn');
+// // btn.style.background = 'red';
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     // e.target gives the element 
+//     // console.log(e.target)
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     ul.lastElementChild.innerHTML = '<h1>HELO</h1>'
+// })
+
+//TESTING
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please Enter All Fields'
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        //Clear Fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+
+}
 
