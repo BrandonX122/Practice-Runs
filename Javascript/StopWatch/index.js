ab10dc9613affa92339337f12865b7f2275faf08
Brandon Nascimento <brandonnascimento112@gmail.com>
2025-05-30 22:36:56 -0400

    Added Lots of Pomodoro logic, little tweaks left

--- Javascript/StopWatch/index.js
+++ Javascript/StopWatch/index.js
@@ -1,9 +1,85 @@
 const display = document.querySelector('.display-timer');
 const buttons = document.querySelectorAll('.btn');
+const settingBtn = document.querySelector('#setting-btn');
+const settingDisplay = document.querySelector('.settings');
+const saveBtn = document.querySelector('#save-btn');
+const pomoButtons = document.querySelectorAll('.p-btn');
+const pomodoroInput = document.querySelector('#pomodoro-input');
+const shortInput = document.querySelector('#short-input');
+const longInput = document.querySelector('#long-input');
+const pomoDisplay = document.querySelector('.pomodoro-timer');
 let [milseconds, seconds, minutes, hours] = [0,0,0,0];
-//equal to setting multiple variable into a value.
+let [pomoMinutes, shortMinutes, longMinutes] = [
+    pomodoroInput.value, shortInput.value, longInput.value    
+]
+let timerDuration = null;
 let timer = null;
+let pomoTimer = null;
 
+//POMODORO
+settingBtn.addEventListener('click', (e) => {
+    settingDisplay.classList.add('show');
+})
+
+saveBtn.addEventListener('click', (e) => {
+    pomoMinutes = pomodoroInput.value;
+    // shortMinutes = shortInput.value;
+    // longMinutes = longInput.value;
+
+    const pm = convertToMin(pomoMinutes);
+    // const sm = convertToMin(shortMinutes);
+    // const lm = convertToMin(longMinutes);
+
+    pomoDisplay.innerHTML = pm;
+
+    settingDisplay.classList.remove('show');
+})
+
+pomoButtons.forEach((p_button) => {
+    p_button.addEventListener('click', (e) => {
+        if(p_button.id === 'p-start') {
+            if(pomoTimer) {
+                clearInterval(pomoTimer);
+            }
+            const timerMin = parseInt(pomoDisplay.innerHTML.split(":")[0]);
+            const timerSec = parseInt(pomoDisplay.innerHTML.split(":")[1]);
+            timerDuration = (((timerSec / 60) + timerMin) * 60);
+
+            pomoTimer = setInterval(updatePomodoro, 1000);
+            document.querySelector('#p-start').disabled = true;
+        } else if(p_button.id === 'p-pause') {
+            clearInterval(pomoTimer);
+            document.querySelector('#p-start').disabled = false;
+        }
+    })
+});
+
+function updatePomodoro () {
+    timerDuration--;
+
+    if(timerDuration < 0) {
+        clearInterval(pomoTimer);
+        pomoDisplay.innerHTML = "00 : 00";
+        document.querySelector('#p-start').disabled = false;
+    } else {
+        let value = timerDuration / 60;
+        pomoDisplay.innerHTML = `${convertToMin(value)}`
+    }
+
+}
+
+function convertToMin(time) {
+    const minutes = Math.floor(time);
+    const seconds = Math.round((time - minutes) * 60);
+
+    let m = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    let s = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${m} : ${s}`;
+}
+
+
+//TIMER
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         if(button.id === 'start-btn'){
@@ -19,6 +95,12 @@ buttons.forEach((button) => {
             [milseconds, seconds, minutes, hours] = [0,0,0,0];
             display.innerHTML = "00 : 00 : 00 : 000";
         }
+
+        buttons.forEach((btn) => {
+            btn.classList.remove('pressed');
+        });
+        //Apply pressed classed only onto currently pressed buttons
+        button.classList.add('pressed');
     }) 
 });
 
