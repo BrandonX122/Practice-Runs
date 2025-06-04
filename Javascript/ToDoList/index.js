d9a699f8b5b9c68f66aee35dabdf8315a8e353ea
Brandon Nascimento <brandonnascimento112@gmail.com>
2025-06-04 19:26:19 -0400

    Create new edit function to edit todos in place

--- Javascript/ToDoList/index.js
+++ Javascript/ToDoList/index.js
@@ -38,6 +38,16 @@ function updateTodoList() {
     })
 }
 
+function editTodo(field, index) {
+    field.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+            allTodos[index].text = field.value;
+            saveTodos();
+            updateTodoList();
+        }
+    });
+}
+
 function createTodoItem(todo, todoIndex) {
     const todoID = "todo-" + todoIndex;
     const todoLI = document.createElement('li');
@@ -53,6 +63,11 @@ function createTodoItem(todo, todoIndex) {
                 <label for="${todoID}" class="todo-text">
                     ${todoText}
                 </label>
+                <button class="edit-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--secondary-color)>
+                    <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
+                </svg>
+                </button>
                 <button class="delete-button">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--secondary-color)>
                         <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
@@ -62,13 +77,27 @@ function createTodoItem(todo, todoIndex) {
 
     const deleteButton = todoLI.querySelector('.delete-button');
     deleteButton.addEventListener('click', () => deleteTodoItem(todoIndex));
+
+    const editButton = todoLI.querySelector('.edit-button');
+    editButton.addEventListener('click', () => {
+        const textElement = todoLI.querySelector('.todo-text');
+        const newInput = document.createElement('input');
+        newInput.setAttribute('type', 'text');
+        newInput.id = 'todo-input'
+        newInput.value = todoText;
+        textElement.innerHTML = '';
+        textElement.append(newInput);
+
+        editTodo(newInput, todoIndex);
+    });
+
     const checkbox = todoLI.querySelector('input');
     checkbox.addEventListener('change', (e) => {
         allTodos[todoIndex].completed = checkbox.checked;
         saveTodos();
     });
     checkbox.checked = todo.completed;
-    
+
     return todoLI;
 }
 
@@ -82,3 +111,9 @@ function loadTodos() {
     const todos = localStorage.getItem("todos") || "[]"
     return JSON.parse(todos);
 }
+
+//EDIT FUNCTION
+//SEARCH
+//PIority
+//group task
+//
