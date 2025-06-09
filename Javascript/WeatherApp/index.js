faca7d2a288c07533655c7b3d8cd430b93ad80bb
Brandon Nascimento <brandonnascimento112@gmail.com>
2025-06-09 16:57:11 -0400

    Created simple weather app

---
+++ Javascript/WeatherApp/index.js
@@ -0,0 +1,32 @@
+const apiKey = "e0dd12c9301dc1a5a5a7d1c099c4c11e";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status === 404) {
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+    } else {
+        document.querySelector('.error').style.display = 'none';
+        document.querySelector('.weather').style.display = 'block';
+    }
+    let data = await response.json();
+    //easier to handle HTTP request, fetch() is a get request to the API
+    console.log(data);
+
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°F"; 
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' MPH';
+
+
+}
+
+searchBtn.addEventListener('click', () => {
+    const cityName = searchBox.value;
+    checkWeather(cityName);
+});
