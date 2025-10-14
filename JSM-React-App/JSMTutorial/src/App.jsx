284d85043ee74c8ddae02441facbb8837a0f1a62
Brandon <brandonnascimento112@gmail.com>
2025-10-13 20:41:47 -0400

    added a header for app

--- JSM-React-App/JSMTutorial/src/App.jsx
+++ JSM-React-App/JSMTutorial/src/App.jsx
@@ -1,42 +1,27 @@
-import { useEffect, useState } from "react";
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  return (
-    <div
-      className="card"
-      onClick={() => {
-        setCount(count + 1);
-      }}
-    >
-      <h2>
-        {title} <br /> {count}
-      </h2>
-
-      <button
-        onClick={() => {
-          setHasLiked(!hasLiked);
-        }}
-      >
-        {hasLiked ? "Liked" : "Like"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
+import Search from "./components/Search.jsx";
+import { useState } from "react";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="card-container">
-      <Card title={"Star Wars"} />
-      <Card title={"Avatar"} />
-      <Card title={"The Lion King"} />
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <h1 className="text-white text-3xl">{searchTerm}</h1>
+        </header>
+      </div>
+    </main>
   );
 };
 
