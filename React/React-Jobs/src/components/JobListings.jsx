4bb55567e28c3526cd216ec2a7ee2e841b302254
Brandon <brandonnascimento112@gmail.com>
2025-10-22 21:47:41 -0400

    Completed Tutorial

---
+++ React/React-Jobs/src/components/JobListings.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import JobListing from "./JobListing.jsx";
+import Spinner from "./Spinner.jsx";
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const BASE_URL = "/api/jobs";
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  const fetchJobs = async () => {
+    try {
+      const apiUrl = isHome ? `${BASE_URL}?_limit=3` : BASE_URL;
+
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setJobs(data || []);
+    } catch (error) {
+      console.log("Failed to fetch jobs", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
+        </h2>
+        {isLoading ? (
+          <Spinner loading={isLoading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default JobListings;
