f6c4e39bba6537904fdc4182c2a9515056b08bd1
Brandon <brandonnascimento112@gmail.com>
2025-11-05 18:23:41 -0500

    Setup function to grab access token and made a tester function to retrieve data from API

--- React/React-Music/src/App.jsx
+++ React/React-Music/src/App.jsx
@@ -12,6 +12,54 @@ import {
 } from "react-router-dom";
 
 const App = () => {
+  const BASE_URL = "https://api.spotify.com";
+  const { VITE_SPOTIFY_CLIENT_ID, VITE_SPOTIFY_CLIENT_SECRET } = import.meta
+    .env;
+  const [accessToken, setAccessToken] = useState(null);
+
+  //acquire access token from Spotify Web API
+  useEffect(() => {
+    async function getAccessToken() {
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          grant_type: "client_credentials",
+          client_id: VITE_SPOTIFY_CLIENT_ID,
+          client_secret: VITE_SPOTIFY_CLIENT_SECRET,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to pass credentials for access token");
+      }
+      const data = await response.json();
+      setAccessToken(data.access_token);
+    }
+    getAccessToken();
+  }, []);
+
+  async function getArtist() {
+    const res = await fetch(`${BASE_URL}/v1/artists/4Z8W4fKeB5YxbusRsdQVPb`, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to retrieve artist");
+    }
+    const data = await res.json();
+    console.log(data);
+  }
+  useEffect(() => {
+    if (!accessToken) return;
+    getArtist();
+  }, [accessToken]);
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
